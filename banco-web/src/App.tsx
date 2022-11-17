7e064d3b82c493636f135777678a2e86c97fbd98
GustavoRocha <rocha.gustavo@escolar.ifrn.edu.br>
2022-11-17 19:52:50 -0300

    reat

--- banco-web/src/App.tsx
+++ banco-web/src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import LoginPage from "./loginPage/login-page";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='loginPage'>
+      <p className='esfera1'></p>
+      <h1> Welcome to Test Bank</h1>
+      <p className='aquiestamos'>Aqui estamos criando e inovando</p>P
+      <p className='esfera2'></p>
+      <p className='esfera3'></p>
+      <div className='retangulo'>
+        <LoginPage/>
+      </div>
+  </div>
   );
 }
 
---
+++ banco-web/src/loginPage/login-page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/login.css';
+
+let LoginPage = function (){
+    return(
+        <p>Gustavo</p>
+    );
+
+}
+export default LoginPage;
