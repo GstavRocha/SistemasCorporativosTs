a6431f28704535cd438d805b645633ffae24895c
GustavoRocha <rocha.gustavo@escolar.ifrn.edu.br>
2022-11-22 19:35:22 -0300

    animatacao

--- banco-web/apps/banco-web/src/App.tsx
+++ banco-web/apps/banco-web/src/App.tsx
@@ -6,11 +6,11 @@ import LoginPage from "./loginPage/login-page";
 function App() {
   return (
     <div className='loginPage'>
-      <p className='esfera1'></p>
+      <p className='esfera1 esferaAnimation1'></p>
       <h1> Welcome to Test Bank</h1>
       <p className='aquiestamos'>Aqui estamos criando e inovando</p>P
-      <p className='esfera2'></p>
-      <p className='esfera3'></p>
+      <p className='esfera2 esferaAnimation1'></p>
+      <p className='esfera3 esferaAnimation1'></p>
       <div className='retangulo'>
         <LoginPage/>
       </div>
