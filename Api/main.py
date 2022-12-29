e189796f728bf5cef0031cad858c01f28ef8b314
GustavoRocha <rocha.gustavo@escolar.ifrn.edu.br>
2022-12-29 07:56:14 -0300

    peewee

--- Api/main.py
+++ Api/main.py
@@ -4,9 +4,19 @@ from sqlalchemy.orm import Session
 from sqlRelation.db import engine, SessionLocal
 from sqlRelation.DataBase import models, schemas
 from sqlRelation.Routes import CRUD
+models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
-
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 @app.get("/")
 async def serveLoad():
         return {"Servidor": "Online"}
+@app.get("/usuarios/", response_model=list[schemas.Correntista_Schema])
+async def getUsuarios(s: int = 0, l: int = 100, db: Session = Depends(get_db)):
+    usuarios = CRUD.getTodos(db, skip=s, limit=l)
+    return usuarios
 
--- Api/sqlRelation/DataBase/schemas.py
+++ Api/sqlRelation/DataBase/schemas.py
@@ -1,55 +1,6 @@
 from typing import List
+import peewee
 from pydantic import BaseModel
 
-
-class Correntista_Schema(BaseModel):
-    id: int
-    nome: str
-    email: str
-    saldo: float
-
-
-class Movimentacao_Schema(BaseModel):
-    id: int
-    cod: int
-    tipo: str
-    valor: float
-    data: int
-class Correntista(BaseModel):
-    id: int
-    nome: str
-
-
-class Saldo(Correntista_Schema):
-    id: int
-    nome: str
-    saldo: float
-
-    class Config:
-        orm_mode = True
-
-
-class Transferencia(Movimentacao_Schema):
-    id: int
-    nome: str
-    cod: int
-    valor: float
-
-    class Config:
-        orm_mode = True
-
-
-class Deposito(Movimentacao_Schema):
-    id: int
-    nome: str
-    valor: float
-
-    class Config:
-        orm_mode = True
-class listaUsuario(Correntista):
-    id: int
-    nome: str
-
-    usuarios: list[Correntista_Schema] = []
-    class Config:
-        orm_mode = True
+class PeeweeGet(GetterDict):
+#https://fastapi.tiangolo.com/pt/advanced/sql-databases-peewee/?h=schemas#create-a-peeweegetterdict-for-the-pydantic-models-schemas
